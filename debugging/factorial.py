8b6312f2ad096348582f1c5020c07b7bf609246a
Juandicode <juandiegomhs@mail.com>
2025-01-15 11:53:00 -0300

    python

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,19 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrementar n en cada iteración
+        n -= 1
     return result
 
-# Asegurarse de que se pase un argumento y manejar posibles errores
-if len(sys.argv) > 1:
-    try:
-        num = int(sys.argv[1])
-        if num < 0:
-            print("Error: El factorial no está definido para números negativos.")
-        else:
-            f = factorial(num)
-            print(f"Factorial de {num}: {f}")
-    except ValueError:
-        print("Error: Por favor, ingrese un número entero válido.")
-else:
-    print("Uso: ./script.py <número>")
+f = factorial(int(sys.argv[1]))
+print(f)
